e90fb9865b815f2a63d729d5705b6c00a307215f
afguerrerogu <afguerrerogu@unal.edu.co>
2019-12-06 13:30:57 -0500

    codigo

--- profin.cpp
+++ profin.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 #include <vector>
 
-std::vector<double> y = {2.5, 0.0, 0.0 , 0.0 , 2.355, 0.0 }; // {x0, y0, z0, px , py, pz} 
+std::vector<double> y = {2.5, 0 , 0 , 0 , 1.68884, 0.2 }; // {x0, y0, z0, px , py, pz} 
 
 const double A= 1.0;
 const double C= 1.0;
 const double w= 0.25;
 const double a= 1.25;
 const double b= 1.0;
-const double c= 0.25;
+const double c= 0.75;
 
 double f(double t, const std::vector<double>  y, int id);
 void rk4(double ta, double tb, double h, std::vector<double> & y);
 
 int main(void){
 
-    rk4(0, 100, 0.01, y);
+    rk4(0, 100000 , 0.1, y);
     return 0;
 }
 
@@ -48,6 +48,8 @@ double f(double t, const std::vector<double>  y, int id){
 
 void rk4(double ta, double tb, double h, std::vector<double> & y)
 {
+  std::cout.precision(15);
+
   std::vector<double> k1, k2, k3, k4, aux;
   k1.resize(y.size());
   k2.resize(y.size());
@@ -58,10 +60,13 @@ void rk4(double ta, double tb, double h, std::vector<double> & y)
   const int N = (tb-ta)/h;
   for (int nt = 0; nt < N; ++nt) {
     double t = ta + h*nt;
+    
     // k1
     for(int ii = 0; ii < y.size(); ++ii) {
       k1[ii] = h*f(t, y, ii);
     }
+
+    
     // k2 aux
     for(int ii = 0; ii < y.size(); ++ii) {
       aux[ii] = y[ii] + k1[ii]/2;
@@ -70,6 +75,8 @@ void rk4(double ta, double tb, double h, std::vector<double> & y)
     for(int ii = 0; ii < y.size(); ++ii) {
       k2[ii] = h*f(t + h/2, aux, ii);
     }
+    
+
     // k3 aux
     for(int ii = 0; ii < y.size(); ++ii) {
       aux[ii] = y[ii] + k2[ii]/2;
@@ -78,6 +85,8 @@ void rk4(double ta, double tb, double h, std::vector<double> & y)
     for(int ii = 0; ii < y.size(); ++ii) {
       k3[ii] = h*f(t + h/2, aux, ii);
     }
+    
+
     // k4 aux
     for(int ii = 0; ii < y.size(); ++ii) {
       aux[ii] = y[ii] + k3[ii];
@@ -85,11 +94,13 @@ void rk4(double ta, double tb, double h, std::vector<double> & y)
     //k4
     for(int ii = 0; ii < y.size(); ++ii) {
       k4[ii] = h*f(t + h, aux, ii);
-    }
+    } 
+
     // write new y
     for(int ii = 0; ii < y.size(); ++ii) {
       y[ii] = y[ii] + (k1[ii] + 2*k2[ii] + 2*k3[ii] + k4[ii])/6.0;
     }
-    std::cout << t << "\t" << y[0] << "\t" << y[1]  << "\t" << y[2]<< std::endl;
+   std::cout  /* << t << "\t\t\t" */ << y[0] << "\t" << y[1] <<"\t" << y[2] /* <<"\t\t" << y[3] <<"\t\t" << y[4] <<"\t\t" << y[5] */ << std::endl;
+   
   }
 }
